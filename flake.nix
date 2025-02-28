65d21c626372ecd330511e393cdf91d63af907da
Adam Ruzicka <a.ruzicka@outlook.com>
2025-02-28 17:59:34 +0100

    Flake scaffolding

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Packages for things that are not in nixpkgs yet";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+      }
+    );
+}
