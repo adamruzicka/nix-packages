f464a962ed52a781c0f16c55c7dcaa377aaccb18
Adam Ruzicka <a.ruzicka@outlook.com>
2025-08-14 21:01:19 +0200

    Migrate to snowfall-lib

--- flake.nix
+++ flake.nix
@@ -1,22 +1,24 @@
 {
   description = "Packages for things that are not in nixpkgs yet";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        packages.broker = pkgs.callPackage ./packages/broker.nix { };
-      }
-    );
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      # You must provide our flake inputs to Snowfall Lib.
+      inherit inputs;
+
+      src = ./.;
+
+      snowfall = {
+        root = ./.;
+      };
+    };
 }
