1499c6378246ed68cf255d996bd29f708237d46f
Adam Ruzicka <a.ruzicka@outlook.com>
2025-09-02 10:04:25 +0200

    Drop flake-utils

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
     snowfall-lib = {
       url = "github:snowfallorg/lib";
       inputs.nixpkgs.follows = "nixpkgs";
